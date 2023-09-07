3c3c79e9b780fa183e3d89e8001a6e43b42149f7
Gezzer Garcia <gerzeus@gmail.com>
2023-09-07 13:48:45 -0600

    respuesta 1

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express');
+const multer = require('multer');
 const cors = require('cors');
 require('dotenv').config();
 const log = require('debug')('app');
 
 const app = express();
 
+// Configura multer para gestionar la carga de archivos
+const storage = multer.memoryStorage(); // Almacena el archivo en la memoria
+const upload = multer({ storage });
+
 app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', (req, res) => {
   res.sendFile(`${process.cwd()}/views/index.html`);
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  log('/api/fileanalyse');
+  if (req.file) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } else {
+    res.status(400).json({ error: 'No se proporcionó ningún archivo.' });
+  }
 });
 
 const port = process.env.PORT || 3000;
